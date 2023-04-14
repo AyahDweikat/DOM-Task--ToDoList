cdad7a3ed9916ca3566ec18891b13bfe46f37411
AyahDweikat <dweikatayah99@gmail.com>
2023-04-14 04:06:51 +0300

    to do list - new todo, delete, counter do& undoo, change between do&undo, search

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 let taskList = [
-//   {
-    // id:1
-//     taskInput: "task 1",
-//     asigneeInput: "ayah",
-//     doneState: false,
-//   },
-//   {
-//     taskInput: "task 2",
-//     asigneeInput: "ameen",
-//     doneState: false,
-//   },
+  //   {
+  // id:1
+  //     taskInput: "task 1",
+  //     asigneeInput: "ayah",
+  //     doneState: false,
+  //   },
+  //   {
+  //     taskInput: "task 2",
+  //     asigneeInput: "ameen",
+  //     doneState: false,
+  //   },
 ];
-let count =0;
+let count = 0;
 let genID = generateID();
 let text = "";
 let searchedResult = [];
@@ -21,15 +21,10 @@ let asigneeInput = "";
 let task = document.getElementById("task");
 let asignee = document.getElementById("assignee");
 let submitTask = document.getElementById("submitTask");
-let deleteTask = document.getElementById("deleteTask");
-let changeDoneState = document.getElementById("changeDoneState");
+let search = document.getElementById("search");
 let taskDisplay = document.getElementById("taskDisplay");
 let counterDone = document.getElementById("counterDone");
 let counterUnDone = document.getElementById("counterUnDone");
-let search = document.getElementById("search");
-
-
-
 
 task.addEventListener("keyup", function taskHandler(event) {
   taskInput = event.target.value;
@@ -44,37 +39,40 @@ submitTask.addEventListener("click", function addTaskHandler(event) {
   displayTask(taskList);
   displayCounter(taskList);
 });
-search.addEventListener('keyup', function searchHandler(event){
-    searchedResult=[]
-    if(event.target.value ==""){
-        displayTask(taskList);
-        displayCounter(taskList);
-        return;
-    }
-    taskList.forEach((item)=>{
-        if(item.taskInput.includes(event.target.value) || item.asigneeInput.includes(event.target.value)){
-            searchedResult.push(item)
-        }
-    })
-    displayTask(searchedResult);
-})
-function changeDoneHandler(event, id){
-    taskList.forEach((item)=>{
-        if(item.id === id){
-            item.doneState = !item.doneState;
-        }
-    })
+search.addEventListener("keyup", function searchHandler(event) {
+  searchedResult = [];
+  if (event.target.value == "") {
     displayTask(taskList);
     displayCounter(taskList);
+    return;
+  }
+  taskList.forEach((item) => {
+    if (
+      item.taskInput.includes(event.target.value) ||
+      item.asigneeInput.includes(event.target.value)
+    ) {
+      searchedResult.push(item);
+    }
+  });
+  displayTask(searchedResult);
+});
+function changeDoneHandler(event, taskList, id) {
+  taskList.forEach((item) => {
+    if (item.id === id) {
+      item.doneState = !item.doneState;
+    }
+  });
+  displayTask(taskList);
+  displayCounter(taskList);
 }
-function deleteHandler(id){
-    taskList.forEach((item, idx)=>{
-        if(item.id === id){
-            taskList.splice(idx,1)
-        }
-    })
-    displayTask(taskList)
-    displayCounter(taskList)
+function deleteHandler(taskList, id) {
+  taskList.forEach((item, idx) => {
+    if (item.id === id) {
+      taskList.splice(idx, 1);
+    }
+  });
+  displayTask(taskList);
+  displayCounter(taskList);
 }
 function addTask(taskInput, asigneeInput) {
   let objTask = {
@@ -88,63 +86,67 @@ function addTask(taskInput, asigneeInput) {
 function clearInput() {
   task.value = "";
   asignee.value = "";
-  taskInput="";
-  asigneeInput="";
+  taskInput = "";
+  asigneeInput = "";
 }
 function displayTask(list) {
-    // taskDisplay.innerHTML="";
-    list.forEach((item, idx) => {
-        const para1 = document.createElement('p')
-        para1.innerHTML= item.taskInput;
-        const para2 = document.createElement('p')
-        para2.innerHTML= item.asigneeInput;
+  taskDisplay.innerHTML = "";
+  list.forEach((item) => {
+    const taskParag = document.createElement("p");
+    taskParag.innerHTML = item.taskInput;
+    const assigneeParag = document.createElement("p");
+    assigneeParag.innerHTML = item.asigneeInput;
 
-        const btn = document.createElement('button')
-        btn.innerHTML= `<i class="fa-solid fa-circle-xmark"></i>`;
-        btn.setAttribute('id', 'deleteTask')
-        btn.addEventListener('click',function deleteHandler(){
-            
-        })
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = `<i class="fa-solid fa-circle-xmark"></i>`;
+    deleteBtn.setAttribute("id", "deleteTask");
+    deleteBtn.addEventListener("click", () => deleteHandler(list, item.id));
 
-        const newDiv = document.createElement('div')
-        newDiv.appendChild(para1);
-        newDiv.appendChild(para2);
-        newDiv.appendChild(btn);
+    const btnDoneState = document.createElement("button");
+    btnDoneState.innerHTML = item.doneState ? (
+      `<i class="fa-solid fa-circle-check"></i>`
+    ) : (
+      `<i class="fa-regular fa-circle-check"></i>`
+    );
+    btnDoneState.addEventListener('click',(event)=> changeDoneHandler(event, list, item.id))
 
+    const newDiv = document.createElement("div");
+    newDiv.appendChild(taskParag);
+    newDiv.appendChild(assigneeParag);
+    newDiv.appendChild(deleteBtn);
+    newDiv.appendChild(btnDoneState);
 
-        const newList = document.createElement('li')
-        newList.appendChild(newDiv);
-        taskDisplay.appendChild(newList);
+    const newList = document.createElement("li");
+    newList.appendChild(newDiv);
+    taskDisplay.appendChild(newList);
 
-        
-        // text += `
-        // <button id="deleteTask" onClick='deleteHandler(${item.id})'><i class="fa-solid fa-circle-xmark"></i></button>
-        // <div id="changeDoneState" onClick="changeDoneHandler(event,${item.id})">
-        // ${
-        //     item.doneState
-        //     ? `<i class="fa-solid fa-circle-check"></i>`
-        //     : `<i class="fa-regular fa-circle-check"></i>`
-        // }
-        // </div>
-        // `;
+    // text += `
+    // <div id="changeDoneState" onClick="changeDoneHandler(event,${item.id})">
+    // ${
+    //     item.doneState
+    //     ? `<i class="fa-solid fa-circle-check"></i>`
+    //     : `<i class="fa-regular fa-circle-check"></i>`
+    // }
+    // </div>
+    // `;
   });
-//   taskDisplay.innerHTML = text;
+  //   taskDisplay.innerHTML = text;
 }
-function displayCounter(taskList){
-    let countUnDone =0;
-    let countDone =0;
-    taskList?.forEach((item) => {
-        if(item.doneState){
-            countDone++;
-        } else {
-            countUnDone++;
-        }
-    })
-    counterDone.innerHTML=countDone;
-    counterUnDone.innerHTML=countUnDone;
-}
-function* generateID(){
-    while(true){
-        yield count++;
+function displayCounter(taskList) {
+  let countUnDone = 0;
+  let countDone = 0;
+  taskList?.forEach((item) => {
+    if (item.doneState) {
+      countDone++;
+    } else {
+      countUnDone++;
     }
+  });
+  counterDone.innerHTML = countDone;
+  counterUnDone.innerHTML = countUnDone;
+}
+function* generateID() {
+  while (true) {
+    yield count++;
+  }
 }
